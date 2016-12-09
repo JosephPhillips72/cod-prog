7a0805ea127c70d7c8462ada9dcae94df84307d0
Killereagles72 <averagejoegamer72@gmail.com>
2016-12-09 12:25:26 -0600

    figuring out JFrame fullscreen

--- src/frame/Frame.java
+++ src/frame/Frame.java
@@ -1,5 +1,21 @@
 package frame;
 
+import javax.swing.*;
+import javax.*;
+
+
 public class Frame {
+	
+	public static void main(String[]args){
+	
+		
+		JFrame f1 = new JFrame();
+		JPanel p1 = new JPanel();
+		f1.setExtendedState(JFrame.MAXIMIZED_BOTH); 
+		f1.setUndecorated(false);
+		f1.setVisible(true);
+		f1.add(p1);
+		
+	}
 
 }
